cf5f2ebe066692f940109bd63639c1f31c12c1a3
ibrahimhbaran <baranibrahimh@gmail.com>
2022-03-04 14:32:09 +0300

    initial commit

---
+++ app/src/main/java/com/play/harrypotter/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.play.harrypotter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.play.harrypotter.ui.theme.HarryPotterTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HarryPotterTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HarryPotterTestTheme {
+    }
+}
